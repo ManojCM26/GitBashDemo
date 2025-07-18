51e7fc6ff168740fe8c1e7ae2c98d24474dfc568
ManojCM26 <manojcm2611@gmail.com>
2025-07-18 16:06:37 +0530

    initialCommit

---
+++ src/main/java/utilities/WebDriverUtilities.java
@@ -0,0 +1,10 @@
+package utilities;
+
+public class WebDriverUtilities {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("WebDriver driver");
+	}
+
+}
---
+++ src/test/java/organization/OrgTests.java
@@ -0,0 +1,7 @@
+package organization;
+
+public class OrgTests {
+	public static void main(String[] args) {
+		System.out.println("orgnization created");
+	}
+}
