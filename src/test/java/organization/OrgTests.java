c486d13b6599702b9541a267a1b9f4931c824852
ManojCM26 <manojcm2611@gmail.com>
2025-07-18 17:56:03 +0530

    organiztn KLR created

--- src/test/java/organization/OrgTests.java
+++ src/test/java/organization/OrgTests.java
@@ -2,6 +2,6 @@ package organization;
 
 public class OrgTests {
 	public static void main(String[] args) {
-		System.out.println("orgnization created");
+		System.out.println("orgnization KLR created");
 	}
 }
