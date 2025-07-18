b32b6c301294d07177359304f696ef777320679a
ManojCM26 <manojcm2611@gmail.com>
2025-07-18 21:26:38 +0530

    working

--- src/test/java/organization/OrgTests.java
+++ src/test/java/organization/OrgTests.java
@@ -3,5 +3,6 @@ package organization;
 public class OrgTests {
 	public static void main(String[] args) {
 		System.out.println("orgnization KLR created");
+		System.out.println("KLR works with cricket academies");
 	}
 }
